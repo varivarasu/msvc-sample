7af5924f3d912cfd040155f02b52cf0afee51701
vaassu <vaassu@gmail.com>
2018-10-23 13:27:46 +0530

    created user object

--- Usermanager.js
+++ Usermanager.js
@@ -1,9 +1,17 @@
-﻿﻿const userstore = require('./Userstore.js')
+﻿const userstore = require('./Userstore.js')
 
 function UserManager() {
 	// User Object looks like this
 	// { id: '1234', name: 'Some Name', createdAt: '12-Oct-2018 11:00:00 AM', friends: [ {User}, {User}... ]
 	// returns User object
+
+	function user(){
+		id:string
+		name:string
+        createdAt:new Date().getTime()
+        friends: new Array()
+	}
+	
 	function getuser(userid) {
 		const user=null;
 		if(!userid) {
@@ -14,7 +22,7 @@ function UserManager() {
 	
 	// return [] User objects
 	function getallusers() {
-
+				return  userstore.getAllUsers()				
 	}
 
 	// returns User object
@@ -55,7 +63,8 @@ function UserManager() {
 	this.getAllUsers = getallusers
 	this.createUser = createuser
 	this.getUserByName = getuserbyname
-	this.addFriends = addfriends	
+	this.addFriends = addfriends
+	this.User = user
 }
 
 module.exports = new UserManager
