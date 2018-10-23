bc7cff55d9be3800d95ca701cc52239a072fe779
mahanteshmb <mbmg.ise@gmail.com>
2018-10-23 15:28:15 +0530

    updated
    updated

--- Loginmanager.js
+++ Loginmanager.js
@@ -1,19 +1,37 @@
-﻿const UM = require('./Usermanager.js')
+const UM = require('./Usermanager.js')
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+	var LocalStorage = require('node-localstorage').LocalStorage;
+	localStorage = new LocalStorage('./scratch');
+//	localStorage.clear;
+  }
+   
+  
+  
 
 function LoginManager() {
 	// Returns User object
+	
 	function getcurrentuser() {
-
+		return localStorage.getItem('userObj');
 	}
 
 	// Return User object
 	function signup(user) {
-		return UM.createUser(user)
+		var uo = UM.createUser(user);
+		//var uo = user;
+		localStorage.setItem('userObj',uo);
+		return uo;
+		
 	}
 
 	// Return User object or null
 	function signin(username) {
 
+		var uo = UM.getuserbyname(username);
+		//var uo = {name:username};
+		localStorage.setItem('userObj',uo);
+		return uo;
 	}
 
 	this.getCurrentUser = getcurrentuser
