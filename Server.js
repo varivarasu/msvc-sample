692d53189329f6ceb7a4ddb3c30a5743348ec8ea
Raja <raja.sek.veerla@hpe.com>
2018-10-23 15:23:32 +0530

    Post Manager

---
+++ Server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 3100
+
+app.get('/', (req, res) => res.send('Post'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
